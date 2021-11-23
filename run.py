d8460f1a065709beb14fffa4cf2e68037817bd9a
Konstantin <38739711+Mosl1k@users.noreply.github.com>
2021-11-23 09:01:16 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import requests, time
 
 while True:
-    r = requests.get('http://kpalch.herokuapp.com')
+    r = requests.get('http://kpalch.herokuapp.com/about')
     print(r.text)
     time.sleep(3500)
